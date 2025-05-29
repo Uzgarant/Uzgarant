0b2ff0da87cfc9d92e96c82ceb31c63e035e5a21
Uzgarant <wellaco71@gmail.com>
2025-05-29 23:33:24 +0500

    Create config1.py
    Telegram bot

---
+++ config1.py
@@ -0,0 +1,41 @@
+api_id1 = 29684974
+api_hash1 = '12e80be8b4487b75d9452c03bd8d6bfe'
+phone1 = '+998336711190'  # yangi telefon raqami
+
+interval = 80  # soniyada
+
+delay = 5
+
+text1 = """
+Amerika nomer (AQSH)  🇺🇸- 8ming ✅
+
++12545*53568 🇺🇸
++15309*40377 🇺🇸
++18307*92036 🇺🇸
++14303*91178 🇺🇸
++13614*09699 🇺🇸
++12545*53026 🇺🇸
+Starsla bor ⭐️ -
+
+15talik 3.5ming
+25talik 6ming
+50talik 11ming
+
+(Kamini soʻramang)"""
+
+text2 = """
+Amerika nomer (AQSH)  🇺🇸- 8ming ✅
+
++12545*53568 🇺🇸
++15309*40377 🇺🇸
++18307*92036 🇺🇸
++14303*91178 🇺🇸
++13614*09699 🇺🇸
++12545*53026 🇺🇸
+Starsla bor ⭐️ -
+
+15talik 3.5ming
+25talik 6ming
+50talik 11ming
+
+(Kamini soʻramang)"""
